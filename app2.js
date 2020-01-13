53baa87d9a75ed01c0f41d3f41f425665cded672
Nenad <nstokic@gmail.com>
2020-01-13 02:45:40 +0100

    version 3

--- app2.js
+++ app2.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-//const mongodb = require('mongodb');
+//const bodyParser = require("body-parser");
 const mongoConnect = require("./database").mongoConnect;
 const getDb = require('./database').getDb;
 
@@ -9,7 +8,8 @@ const app = express();
 // app.set("view engine", "ejs");
 // app.set("views", "views");
 
-var friendsResponse = [];
+let friendsResponse = [];
+let friendsoffriends = [];
 
 // GET SINGLE USER INFO
 app.get("/user/:userId", (req, res) => {
@@ -19,11 +19,11 @@ app.get("/user/:userId", (req, res) => {
   .find({id: userId})
   .toArray()
   .then(result => { 
-    console.log(result);
     const response = JSON.stringify({
       "Name": result[0].firstName,
       "Surname": result[0].surname,
-      "Age": result[0].age
+      "Age": result[0].age,
+      "id": result[0].id
     });
     res.send(response);
   })
@@ -41,8 +41,8 @@ app.get("/friends/:userId", (req, res) => {
   .then(user => { 
     const friends = user[0].friends;
     console.log(friends);
-    var counter = 0;
-    return new Promise((resolve, reject) => { // obećanje ludom radovanje :D
+    let counter = 0;
+    return new Promise((resolve, reject) => { 
       friends.forEach((friendId, index, array) => {
         db.collection('people')
         .find({id: friendId})
@@ -51,27 +51,16 @@ app.get("/friends/:userId", (req, res) => {
           friendsResponse.push({
            "Name": result[0].firstName,
            "Surname": result[0].surname,
-           "Age": result[0].age
+           "Age": result[0].age,
+           "id": result[0].id
           });
           counter++;
-          console.log("push new friend" + result[0].firstName);
           if(counter === array.length) {
             resolve(JSON.stringify(friendsResponse));
           }
         })
       })
     });
-    
-    // setTimeout(() => {
-    //   console.log(friendsResponse);
-    // }, 5000);
-
-    // const response = JSON.stringify({
-    //   "Name": result[0].firstName,
-    //   "Surname": result[0].surname,
-    //   "Age": result[0].age
-    // });
-    //res.send(JSON.stringify(friendsResponse));
   })
   .then(result => {
     res.send(result);
@@ -79,10 +68,71 @@ app.get("/friends/:userId", (req, res) => {
   .catch(err => console.log(err));
 });
 
-function returnFriends(r) {
-  //console.log(r);
-  //res.send(JSON.stringify(friendsResponse));
-}
+
+// FRIENDS OF FRIENDS NOT DIRECTLY CONNECTED TO USER
+app.get("/friendsoffriends/:userId", (req, res) => {
+  friendsResponse = [];
+  friendsoffriends = [];
+  const userId = parseInt(req.params.userId);
+  const db = getDb();
+  db.collection('people')
+  .find({id: userId})
+  .toArray()
+  .then(user => { 
+    // Direct friends
+    const friends = user[0].friends;
+    console.log("Direct friends of " + userId + ": " + friends);
+    let counter = 0;
+    return new Promise((resolve, reject) => { 
+      friends.forEach((friendId, index, array) => {
+        db.collection('people')
+        .find({id: friendId})
+        .toArray()
+        .then(result => {
+          let fof = result[0].friends.filter(el => {
+            if(!friends.includes(el)) {
+              friendsoffriends.push(el);
+              return el;
+            }
+          });
+          console.log("Friends of " + friendId + ": " + fof);
+          counter++;
+          if(counter === array.length) {
+            let uniqueFriendsOfFriends = [...new Set(friendsoffriends)];
+            resolve(uniqueFriendsOfFriends);
+          }
+        })
+      })
+    });
+  })
+  .then(friends => {
+    console.log("Unique friends of friends: " + friends); // result for id 20: [ 3, 5, 20, 8, 9, 10, 14, 18 ]
+    let counter = 0;
+    return new Promise((resolve, reject) => { 
+      friends.forEach((friendId, index, array) => {
+        db.collection('people')
+        .find({id: friendId})
+        .toArray()
+        .then(result => {
+          friendsResponse.push({
+           "Name": result[0].firstName,
+           "Surname": result[0].surname,
+           "Age": result[0].age,
+           "id": result[0].id
+          });
+          counter++;
+          if(counter === array.length) {
+            resolve(JSON.stringify(friendsResponse));
+          }
+        })
+      })
+    });
+  })
+  .then(result => {
+    res.send(result);
+  })
+  .catch(err => console.log(err));
+});
 
 mongoConnect(() => {
   app.listen(3000);
